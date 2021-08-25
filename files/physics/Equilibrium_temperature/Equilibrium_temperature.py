9de8c7722dfc5d5728b09d9708df6e904db599c5
Mahdi <mahdighadami2@gmail.com>
2021-08-26 00:35:15 +0430

    edit equilibrium_temperature.py

--- files/physics/Equilibrium_temperature/Equilibrium_temperature.py
+++ files/physics/Equilibrium_temperature/Equilibrium_temperature.py
@@ -4,3 +4,9 @@ c_ice = 2.220
 #Specific latent heats
 water_vaporization_SLH = 2264.705
 water_fusion_SLH = 334
+
+first_material_code = input("what is your first material?\n1.water\n2.ice\n")
+first_material_temperature = input("\nwhat is the temperature(celsius)?\n")
+
+second_material_code = input("\nwhat is your second material?\n1.water\n2.ice\n")
+second_material_temperature = input("\nwhat is the temperature(celsius)?\n")
